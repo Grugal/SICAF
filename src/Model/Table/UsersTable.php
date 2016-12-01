979cfb8d4fb08aebf62f6610fcc109cd8d9953b2
Grugal <adauto_thebest@hotmail.com>
2016-12-01 10:10:17 -0300

    Adicionando todo o escopo de teste (falta a base de dados)

---
+++ src/Model/Table/UsersTable.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Users Model
+ *
+ * @method \App\Model\Entity\User get($primaryKey, $options = [])
+ * @method \App\Model\Entity\User newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\User[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\User|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\User[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\User findOrCreate($search, callable $callback = null)
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class UsersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('users');
+        $this->displayField('id');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        return $validator            
+            ->notEmpty('username', 'A username is required')
+            ->notEmpty('password', 'A password is required')
+            ->notEmpty('role', 'A role is required')
+            ->add('role', 'inList', [
+                    'rule' =>['inList', ['Admin', 'Author']],
+                    'message'=> 'Please enter a valid role'
+                ]);
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['username']));
+
+        return $rules;
+    }
+}
---
+++ src/Template/Articles/add.ctp
@@ -0,0 +1,26 @@
+<nav class="large-3 medium-4 columns" id="actions-sidebar">
+    <ul class="side-nav">
+        <li class="heading"><?= __('Actions') ?></li>
+        <li><?= $this->Html->link(__('Listar Requerimentos'), ['action' => 'index']) ?></li>
+    </ul>
+</nav>
+<div class="articles form large-9 medium-8 columns content">
+    <?= $this->Form->create($article) ?>
+    <fieldset>
+        <legend><?= __('Adicionar Requerimento') ?></legend>
+        <?php
+            echo $this->Form->create($article);
+            // just added the categories input
+            echo $this->Form->input('category_id');
+            echo $this->Form->input('aluno');
+            echo $this->Form->input('turno');
+            echo $this->Form->input('periodo');
+            echo $this->Form->input('matricula');
+            echo $this->Form->input('body', ['rows' => '3']);
+            echo $this->Form->button(__('Salvar Requerimento'));
+            echo $this->Form->end();
+        ?>
+    </fieldset>
+    <!-- <?= $this->Form->button(__('Submit')) ?> -->
+    <?= $this->Form->end() ?>
+</div>
